6457da70d5103f9f1dedf0dfa08c03e1409acf80
David Domingo <dj@MacBook-Air.local>
2020-09-11 11:20:35 -0400

    add extention

--- attachments/TimeSignal.c
+++ attachments/TimeSignal.c
@@ -1,15 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// Place any necessary global variables here
+
 void handle_sigfpe(int signum){
-	
+
 	// Handler code goes here
 
 }
 
 int main(int argc, char *argv[]){
-	
-	/* 
+
+	/*
 	 *
 	 * Implement Here
 	 *
